6d45cafc879e3bdc315abed1228ec535e465c046
ms953ssx <ms953@sussex.ac.uk>
2021-02-19 01:14:01 +0000

    cleaned up cli output, count num of labelled tweets and only output unlabelled tweets

--- bin/manual_label.py
+++ bin/manual_label.py
@@ -7,27 +7,28 @@ def main():
     with conn:
         cur = conn.cursor()
         cur.execute("SELECT COUNT(USERID) FROM TWEETS WHERE ISHARASSMENT IS NOT NULL")
-        print(cur.fetchall())
-        cur.execute("SELECT * FROM TWEETS WHERE USERID NOT IN (SELECT USERID FROM TWEETS ORDER BY RANDOM() LIMIT 200)")
+        count = cur.fetchall()[0][0]
+        cur.execute("SELECT * FROM TWEETS WHERE ISHARASSMENT IS NULL ORDER BY RANDOM() LIMIT 200")
 
         for tweet in cur.fetchall():
             tweet_id, user_id, tweet_txt, is_harassment = tweet
+            print("Num of labelled tweets:", count)
             print(tweet_txt, is_harassment)
             x = "blank"
             while x.lower() != 'y' and x.lower() != 'n':
-                x = input("Is this harassment? Y/N: ")
+                x = input("\nIs this harassment? Y/N: ")
                 if x.lower() == 'y':
                     lbl = 1
                 elif x.lower() == 'n':
                     lbl = 0
                 else:
-                    print("invalid input")
+                    print("\ninvalid input\n")
             sql = '''UPDATE TWEETS
                      SET ISHARASSMENT = ?
                      WHERE TWEETID = ? '''
-            print(sql)
             cur.execute(sql, (lbl, tweet_id))
             conn.commit()
+            count +=1
 
 
 
