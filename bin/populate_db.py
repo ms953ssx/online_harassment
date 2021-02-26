00e8d292ba75ef06e4bbccadb852ede38642fecb
ms953ssx <ms953@sussex.ac.uk>
2021-02-26 14:42:29 +0000

    Added check for pronouns in bio as is likely to not be harassment

--- bin/populate_db.py
+++ bin/populate_db.py
@@ -4,6 +4,7 @@ import pandas as pd
 import csv
 import sqlite3
 from datetime import datetime
+import itertools
 
 consumer_key = os.environ.get("CONSUMER_KEY")  # Add your API key here
 consumer_secret = os.environ.get("CONSUMER_SECRET")  # Add your API secret key here
@@ -13,6 +14,13 @@ access_secret = os.environ.get("ACCESS_SECRET")
 homosexual_terms = ['fag', 'faggot', 'fags', 'fudgepacker', 'fudge+packer', 'poofter', 'pansy', 'bender', 'batty', 'ponce', 'dyke', 'rug+muncher', 'lesbo']
 transgender_terms = ['tranny', 'trannie', 'transvestite', 'ladyboy', 'HeShe', 'shemale']
 bisexual_terms = ['switch+hitter', 'gay+for+pay']
+
+#List of pronouns for identification in bio
+pronouns = ["he","she", "him", "her", "his", "hers", "them", "their", "they"]
+cart_pronouns = itertools.product(pronouns, pronouns)
+pronoun_list = []
+for pronoun_prod in cart_pronouns:
+    pronoun_list.append("/".join(pronoun_prod))
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -37,19 +45,21 @@ def main():
                                    q = query,
                                    lang = "en",
                                    tweet_mode="extended",
-                                   since="2020-02-15").items(900):
+                                   since="2020-02-20").items(900):
 
                 #Check if tweet content contains word from bad terms lists
                 is_type_homosexual = 0
                 is_type_transgender = 0
                 is_type_bisexual = 0
+                has_pronouns = 0
                 if any(word in tweet.full_text for word in homosexual_terms):
                     is_type_homosexual = 1
                 if any(word in tweet.full_text for word in transgender_terms):
                     is_type_transgender = 1
                 if any(word in tweet.full_text for word in bisexual_terms):
                     is_type_bisexual = 1
-
+                if any(word in tweet.user.description for word in pronoun_list):
+                    has_pronouns = 1
                 sql = '''
                     INSERT OR REPLACE INTO TWEETS(TWEETID, USERID, TWEET, ISTYPEHOMOSEXUAL, ISTYPETRANSGENDER, ISTYPEBISEXUAL) \
                     VALUES (?,?,?,?,?,?)
